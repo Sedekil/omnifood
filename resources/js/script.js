73fbff6768e156147d522fd121f4cfb2da45162c
Sedekil <chaosdrake@web.de>
2020-04-25 09:31:20 +0200

    nav now responsive

--- resources/js/script.js
+++ resources/js/script.js
@@ -11,9 +11,6 @@ $(document).ready(function() {
         offset: '60px;'
     });
 
-
-
-
     /* Scroll on buttons */
     $('.js--scroll-to-plans').click(function (){
         $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
@@ -60,6 +57,28 @@ $('.js--wp-4').waypoint(function(direction) {
     $('.js--wp-4').addClass('animated pulse');
 }, {
     offset: '50%'
+});
+
+/* Mobile nav */
+$('.js--nav-icon').click(function() {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+
+    nav.slideToggle(200);
+    if (icon .hasClass('ion-navicon-round')) {
+        icon.addClass('ion-close-round');
+        icon.removeClass('ion-navicon-round');
+    } else {
+        icon.addClass('ion-navicon-round');
+        icon.removeClass('ion-close-round');
+    }
+
+
+
+
+    ion-close-round
+
+
 });
 
 
